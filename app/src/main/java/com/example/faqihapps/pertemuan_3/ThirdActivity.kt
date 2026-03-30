43583bce5517cfe37dbc509d03fdbd6f7beb08c0
lab151-20 <faqihhidayah05@gmail.com>
2026-03-30 10:15:40 +0700

    Pertemuan 3

---
+++ app/src/main/java/com/example/faqihapps/pertemuan_3/ThirdActivity.kt
@@ -0,0 +1,39 @@
+package com.example.faqihapps.pertemuan_3
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.faqihapps.R
+import com.example.faqihapps.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+//        val btnKirim : Button = findViewById( R.id.btnKirim)
+//        val noTujuan : EditText = findViewById( R.id.inputNoTujuan)
+
+        binding.btnKirim.setOnClickListener {
+            //Mengambil value dari inputNama dan menampilkan di Logcat
+//            val nomor = binding.inputNoTujuan.text
+//            Toast.makeText(this, "Pesan berhasil dikirim ke $nomor", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ThirdResultActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
